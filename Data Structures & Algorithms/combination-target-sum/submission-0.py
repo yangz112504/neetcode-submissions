070baff12de6638d23e1deb5d609e4958a0e8b41
Zai Y <yangz112504@gmail.com>
2026-08-12 22:52:48 -0700

    Add: combination-target-sum - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum/submission-0.py
@@ -0,0 +1,24 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        # all positive, so we can add and if it's over it's over
+        totalCombos = []
+        currCombos = []
+        def helper(index,totalSum,totalCombos,currCombos):
+            if index == len(nums):
+                return
+
+            if totalSum == target:
+                totalCombos.append(currCombos.copy())
+                return
+            if totalSum > target:
+                return
+            
+            for i in range(index,len(nums)):
+                currCombos.append(nums[i])
+                helper(i, totalSum + nums[i], totalCombos, currCombos)
+                currCombos.pop()
+        
+        helper(0,0,totalCombos,currCombos)
+        return totalCombos
+
+        
