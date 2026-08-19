e60f1b97fd73855c4b2ffc7d74fe7baf724d1c62
Zai Y <yangz112504@gmail.com>
2026-08-18 21:09:33 -0700

    Add: network-delay-time - submission-0

---
+++ Data Structures & Algorithms/network-delay-time/submission-0.py
@@ -0,0 +1,32 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        # when would it be possible for nodes to receive signal vs not?
+        # 
+        # if minheap still has stuff in it return -1?
+        adj = {}
+        for i in range(1,n+1):
+            adj[i] = []
+        
+        for ui,vi,ti in times:
+            adj[ui].append([vi,ti])
+
+        # shortest to do it and then sum it
+        shortest = {}
+        minHeap = [[0,k]]
+
+        while minHeap:
+            cost1, node1 = heapq.heappop(minHeap)
+            if node1 in shortest:
+                continue
+            shortest[node1] = cost1
+        
+
+            for node2, cost2 in adj[node1]:
+                if node2 not in shortest:
+                    heapq.heappush(minHeap, [cost1 + cost2,node2])
+            
+        return max(shortest.values()) if len(shortest) == n else -1
+        
+
+
+        
